c75582916cb1ef9cdd0e9176f49c42113f30bd32
connortodd21 <connortodd21@gmail.com>
2025-08-16 14:33:28 -0700

    Add process comments

--- offline/process_and_generate_statewide_election_results.py
+++ offline/process_and_generate_statewide_election_results.py
@@ -6,12 +6,19 @@ writeIntermediateResults = getWriteIntermediateResultsInput()
 
 data_processor = STATES_TO_DATA_PROCESSOR[state]()
 
+# get raw data
 data = data_processor.getRawData()
+# cleanup data
 data = data_processor.cleanData(data)
+# filter unused rows
 data = data_processor.filterData(data)
+# drop unused columns
 data = data_processor.dropData(data)
+# write intermediate results if desired
 if writeIntermediateResults:
     data_processor.setIntermediateData(data)
     data_processor.writeIntermediateDataset()
+# generate county by county results
 data = data_processor.generateCountyByCountyResults(data).sort_values([data_processor.YEAR, data_processor.ELECTION])
+# save data to db
 data_processor.writeDataToResults(data)
