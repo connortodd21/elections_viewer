d42f9a86327df5266d6bfac523bc9f2a5a134d99
connortodd21 <connortodd21@gmail.com>
2025-09-03 13:11:46 -0700

    Add endpoint to get county results for a year

--- backend/app/api/counties.py
+++ backend/app/api/counties.py
@@ -8,6 +8,7 @@ from app.errors.error_handler import error_response
 from dataloader.counties_dataloader import get_counties_by_state as dataloader_get_counties_by_state
 from dataloader.counties_dataloader import get_county
 from dataloader.election_results_dataloader import get_election_results_for_county as dataloader_get_election_results_for_county
+from dataloader.election_results_dataloader import get_election_results_for_county_and_year as dataloader_get_election_results_for_county_and_year
 from dataloader.election_results_dataloader import get_statewide_election_years_for_county
 from exceptions.InputException import *
 from exceptions.DataNotGeneratedException import *
@@ -34,7 +35,7 @@ def get_counties_by_state(state: str) -> Response:
 
 
 @bp.route('/get_election_results_for_county/<string:fips>/<string:county_name>', methods=['GET'])
-def get_election_results_for_county(county_name: str, fips:str) -> Response:
+def get_election_results_for_county(county_name: str, fips: str) -> Response:
     """
     Given a county name and fips, get the historical election data for that county
     """
@@ -62,8 +63,37 @@ def get_election_results_for_county(county_name: str, fips:str) -> Response:
     except InvalidInputError as e:
         return error_response(400, e.message)
 
+@bp.route('/get_election_results_for_county_and_year/<string:fips>/<string:county_name>/<string:year>', methods=['GET'])
+def get_election_results_for_county_and_year(county_name: str, fips: str, year: str) -> Response:
+    """
+    Given a county name and fips, get the election data for that county and year
+    """
+    try:
+        # input validation 
+        if not isinstance(county_name, str) or not isinstance(fips, str):
+            raise InvalidInputError("Input must be a string")
+
+        county_name = capitalizeCountyName(county_name)
+
+        # get the state the county belongs to
+        county_df = get_county(county_name, fips)
+        state = county_df[STATE].iloc[0]
+        county = county_df[NAME].iloc[0]
+
+        # read elections data from db
+        election_results = dataloader_get_election_results_for_county_and_year(county, state, year)
+
+        # format (make all names capitalized)
+        json_data = dfToJson(election_results)
+
+        return Response(json_data, mimetype='application/json')
+    except DataNotGeneratedException as e:
+        return error_response(500, e.message)
+    except InvalidInputError as e:
+        return error_response(400, e.message)
+
 @bp.route('/get_county_election_years/<string:fips>/<string:county_name>', methods=['GET'])
-def get_county_statewide_election_years(county_name: str, fips:str) -> Response:
+def get_county_statewide_election_years(county_name: str, fips: str) -> Response:
     """
     Given a county name and fips, get the years there was a statewide election
     """
--- backend/dataloader/election_results_dataloader.py
+++ backend/dataloader/election_results_dataloader.py
@@ -12,6 +12,13 @@ def get_election_results_for_county(county: str, state: str) -> pd.DataFrame:
     except FileNotFoundError:
         raise DataNotGeneratedException(county)
 
+def get_election_results_for_county_and_year(county: str, state: str, year: str) -> pd.DataFrame:
+    try:
+        df = readCsv(f"{getGitRoot()}/{RESULTS_FILE_PATH.format(state)}/statewide_results.csv", converters={YEAR: str})
+        return df[(df[COUNTY] == county) & (df[YEAR] == year)]
+    except FileNotFoundError:
+        raise DataNotGeneratedException(county)
+
 def get_statewide_election_years_for_county(county: str, state: str) -> pd.DataFrame:
     try:
         df = readCsv(f"{getGitRoot()}/{RESULTS_FILE_PATH.format(state)}/statewide_results.csv")
