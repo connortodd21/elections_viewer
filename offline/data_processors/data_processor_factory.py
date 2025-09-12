b05f8447b95476b8ee74cb80a68f4c385b5369f9
connortodd21 <connortodd21@gmail.com>
2025-09-12 15:32:23 -0700

    reorganize data processors

--- offline/data_processors/data_processor_factory.py
+++ offline/data_processors/data_processor_factory.py
@@ -1,7 +1,7 @@
 from common.defs import *
-from data_processors.impl.PopulationTrendsByCounty import PopulationTrendsByCountyDataProcessor
-from data_processors.impl.CA import CAStateWideElectionDataProcessor
-from data_processors.impl.MI import MIStateWideElectionDataProcessor
+from data_processors.impl.demographic.PopulationTrendsByCounty import PopulationTrendsByCountyDataProcessor
+from data_processors.impl.state.CA import CAStateWideElectionDataProcessor
+from data_processors.impl.state.MI import MIStateWideElectionDataProcessor
 
 """
 Contains a mapping of all states (using 2-letter abbreviation) to their associated data processor class
